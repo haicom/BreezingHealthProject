702a2029b87e0dd70dfe6e5778aee865fd2391c7
cemlyzone <474376001@qq.com>
2013-09-08 10:06:14 +0800

    Add UITableView Lib Project

---
+++ BreezingHealth/gen/com/breezing/health/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.breezing.health;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ BreezingHealth/src/com/breezing/health/ui/activity/SettingsActivity.java
@@ -0,0 +1,40 @@
+package com.breezing.health.ui.activity;
+
+import com.breezing.health.R;
+
+import android.os.Bundle;
+
+public class SettingsActivity extends ActionBarActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentFrame(R.layout.activity_settings);
+        initValues();
+        initViews();
+        valueToView();
+        initListeners();
+    }
+
+    private void initValues() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void initViews() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void valueToView() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    private void initListeners() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
